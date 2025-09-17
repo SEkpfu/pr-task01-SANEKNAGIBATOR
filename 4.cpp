640a242c95e632cb702438b35f089ea2d66f3c0a
SANEKNAGIBATOR <poltav.sa37@gmail.com>
2025-09-17 11:04:35 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+        double zp, hours, prem;
+        double total, nalog, end;
+        cin >> zp >> hours >> prem;
+        cout << total=zp*hours + ((zp * hours ) * (prem * 100%)) << endl;
+        cout << nalog = total * 0.13 << endl;
+        cout << end = total - nalog << endl;
+}
